b40a96c56b5ad0ef5fc4784ede321a903b5b5f99
Monica Howard <jay.m.howard@gmail.com>
2017-12-25 23:17:32 -0600

    messing around with colors in js

--- gradient-background/script.js
+++ gradient-background/script.js
@@ -3,6 +3,6 @@ var doc, bod, htm;
 addEventListener('load', function() {
   doc = document; bod = doc.body; htm = doc.documentElement;
   addEventListener('scroll', function() {
-    doc.querySelector('#home-page').style.backgroundColor = htm.scrollTop > 199 ? 'rgb(86, 238, 244)' : 'rgb(50, 232, 117)';
+    doc.querySelector('#home-page').style.backgroundColor = htm.scrollTop > 400 ? 'rgb(86, 238, 244)' : 'rgb(50, 232, 117)';
   });
 });
